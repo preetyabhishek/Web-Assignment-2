8a5d8b9f63881a8fb13a59995e39f183177f728a
Mandeep Singh <mandeepsingh@Mandeeps-MacBook-Air.local>
2022-08-05 21:44:03 -0400

    final project

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+
+        [HttpPost("/items")]
+        public ActionResult Create(string description)
+        {
+            Item myItem = new Item(description);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
